91dbee1d9ab481846b0b52a9f2ea81ad23324497
Davi Martins <davibarroso12399@gmail.com>
2026-06-10 11:48:05 -0300

    Criei o loop que faz com que não tenha fórmulas atômicas repetidas na minha lista

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ print("Obs: Não se esqueça dos parênteses.\n")
 user_input = input("Informe a fórmula:")
 list_of_atomic = []
 
+#Só pega s fórmulas atômicas e coloca numa lista
 for char in user_input:
     if char == "^":
         continue
@@ -30,13 +31,28 @@ for char in user_input:
 
 
 atomics = []
+
+#Tira as fórmulas atômicas repetidas
 for atomic in list_of_atomic:
-    print("Vou ver isso dps")
+    number_of_comparison = 0
+    #Se não tem nada dentro da lista de fórmulas atômicas não repetidas. Coloca.
+    if not atomics:
+        atomics.append(atomic)
+    else:
+        #Tendo algo dentro ele passa a comparar se o item da list_of_atomics já está dentro da atomics.
+        for index in range(len(atomics)):
+            #Checa se os itens são diferentes.
+            if atomics[index] != atomic:
+                #aumenta a contagem de diferenças
+                number_of_comparison += 1
+                #Se a contagem de diferença for igual ao tamanho da lista significa que o item de list_of_atomics
+                #É diferente a todos os itens da lista atomics
+                #Então adiciona esse item da list_of_atomic a lista atomics.
+                if number_of_comparison == len(atomics):
+                    atomics.append(atomic)
 
+number_of_rows = 2**len(atomics)
 
-
-number_of_rows = 2**len(list_of_atomic)
-
-print(list_of_atomic)
-print(number_of_rows)
+print(f"Resultado que eu quero: {atomics}")
+print(f"Número de linhas: {number_of_rows}")
 
